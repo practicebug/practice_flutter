b28748b76e937d3d78dc6b91af1426b53c76f05d
practicebug <108483747+practicebug@users.noreply.github.com>
2022-07-12 00:13:05 +0900

    created

--- custom_scroll_view_screen.dart
+++ custom_scroll_view_screen.dart
@@ -1,6 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable_widgets/const/colors.dart';
 
+class _SliverFixedHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final Widget child;
+  final double maxHeight;
+  final double minHeight;
+
+  _SliverFixedHeaderDelegate({
+    required this.child,
+    required this.maxHeight,
+    required this.minHeight,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(
+      child: child,
+    );
+  }
+
+  @override
+  // 최대 높이
+  double get maxExtent => maxHeight;
+
+  @override
+  // 최소 높이
+  double get minExtent => minHeight;
+
+  @override
+  // covariant - 상속된 클래스도 사용가능
+  // oldDelegate - build 가 실행이 됐을때 이전 Delegate
+  // this - 새로운 delegate
+  // shouldRebuild - 새로 build 를 해야할지 말지 결정
+  // false - build 안함, true - build 다시함
+  bool shouldRebuild(_SliverFixedHeaderDelegate oldDelegate) {
+    return oldDelegate.minHeight != minHeight ||
+        oldDelegate.maxHeight != maxHeight ||
+        oldDelegate.child != child;
+  }
+}
+
 class CustomScrollViewScreen extends StatelessWidget {
   final List<int> numbers = List.generate(100, (index) => index);
 
@@ -11,12 +51,61 @@ class CustomScrollViewScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            title: Text('CustomScrollViewScreen'),
-          ),
+          renderSliverAppBar(),
+          renderHeader(),
+          renderBuilderSliverList(),
+          renderHeader(),
+          renderSliverGridBuilder(),
           renderBuilderSliverList(),
         ],
       ),
+    );
+  }
+
+  SliverPersistentHeader renderHeader() {
+    return SliverPersistentHeader(
+      pinned: true,
+      delegate: _SliverFixedHeaderDelegate(
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              '신기하지~',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        minHeight: 75,
+        maxHeight: 150,
+      ),
+    );
+  }
+
+  // AppBar
+  SliverAppBar renderSliverAppBar() {
+    return SliverAppBar(
+      // 스크롤 했을때 리스트의 중간에도 AppBar 가 내려오게 할 수 있음
+      floating: true,
+      // 완전 고정
+      pinned: false,
+      // 자석 효과
+      // floating true 에만 사용 가능
+      snap: true,
+      // 맨 위에서 한계 이상으로 스크롤 했을때
+      // 남는 공간을 차지
+      stretch: true,
+      expandedHeight: 200,
+      collapsedHeight: 150,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.asset(
+          'asset/img/image_1.jpeg',
+          fit: BoxFit.cover,
+        ),
+        title: Text('FlexibleSpace'),
+      ),
+      title: Text('CustomScrollViewScreen'),
     );
   }
 
@@ -47,6 +136,43 @@ class CustomScrollViewScreen extends StatelessWidget {
           );
         },
         childCount: 100,
+      ),
+    );
+  }
+
+  // GridView.count 유사함
+  SliverGrid renderChildSliverGrid() {
+    return SliverGrid(
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                color: rainbowColors[e % rainbowColors.length],
+                index: e,
+              ),
+            )
+            .toList(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+    );
+  }
+
+  // GridView.builder 와 비슷함
+  SliverGrid renderSliverGridBuilder() {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        childCount: 100,
+      ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
       ),
     );
   }
